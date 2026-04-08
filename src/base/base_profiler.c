4cff3181ae9a487317d42361945975eb865e2d6c
omaly <ohnivasasanka@gmail.com>
2026-04-08 15:04:02 +0200

    improved print out log of the profiler

--- src/base/base_profiler.c
+++ src/base/base_profiler.c
@@ -1,4 +1,4 @@
-#include "base_profiler.h"
+#include "base_string.h"
 profiler_block g_profiler_blocks[PROFILER_MAX_BLOCK_COUNT] = {0};
 profiler g_profiler = {0};
 u32 g_profiler_current_parent = PROFILER_NULL_PARENT;
@@ -39,11 +39,23 @@ void profiler_end_and_dump(Arena *arena, s32 fd)
 			{
 				f64 inc_percent = ((f64)b->tsc_inclusive / (f64)total_cpu_elapsed) * 100.0;
 				f64 exc_percent = ((f64)b->tsc_exclusive / (f64)total_cpu_elapsed) * 100.0;
-				str8fmt_write(fd, arena, STR8_LIT("%-15s: %-10u cyc [%5.2f%%]| Exc: [%5.2f%%] | Hits %-8u\n"),
-			    b->name,
-				b->tsc_inclusive, inc_percent,
-			    exc_percent,
-				b->hit_count);
+				if (inc_percent == exc_percent)
+				{
+				    // Note: This block does not have any nested/children blocks
+					str8fmt_write(fd, arena, STR8_LIT("%-20s: %-10u cyc [%5.2f%%] | %-19s | Hits %-8u\n"),
+					b->name,
+					b->tsc_inclusive, inc_percent,
+					STR8_LIT("No children blocks"),
+					b->hit_count);
+				}
+				else
+				{
+				    str8fmt_write(fd, arena, STR8_LIT("%-20s: %-10u cyc [%5.2f%%] | Exclusive: [%5.2f%%] | Hits %-8u\n"),
+			        b->name,
+				    b->tsc_inclusive, inc_percent,
+			        exc_percent,
+				    b->hit_count);
+				}
 			}
 			i += 1;
 		}
